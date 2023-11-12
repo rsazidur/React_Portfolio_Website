c949ca96572216ab233c7bd9c313467ede9ada37
Molla Sazidur Rahman <rsazidur936@gmail.com>
2023-11-12 13:54:32 +0600

    HeaderSocials

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import "./home.css";
+import Me from "../assets/avatar-1.svg";
+import HeaderSocials from './HeaderSocials';
+import ScrollDown from './ScrollDown';
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <section className='home container' id="home">
+            <div className='intro'>
+                <img src={Me} alt='' className='home__img' />
+                <h1 className='home__name'>Molla Sazidur Rahman</h1>
+                <span className='home__eduacation'>I'm a Data Scientist</span>
+
+                <HeaderSocials />
+
+                <a href="#contact" className='btn'>Hire Me</a>
+
+                <ScrollDown />
+            </div>
+        </section>
     )
 }
 
-export default Home
+export default Home;
